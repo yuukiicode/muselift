2169b0e51ed6ef2bf2246b3d6c96a8ca99484f6a
yuukiicode <yuukiicode@proton.me>
2025-11-02 23:03:46 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,14 @@
+import '../styles/globals.css';
+import Layout from '../components/Layout';
+import { AnimatePresence } from 'framer-motion';
+
+export default function App({ Component, pageProps, router }) {
+  return (
+    <Layout>
+      <AnimatePresence mode="wait" initial={false}>
+        <Component {...pageProps} key={router.pathname} />
+      </AnimatePresence>
+    </Layout>
+  );
+}
 
