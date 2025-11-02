79bdc9244102faeea8903e153b097496208ab332
yuukiicode <yuukiicode@proton.me>
2025-11-02 23:00:20 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,1 +1,15 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+export default function Layout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
