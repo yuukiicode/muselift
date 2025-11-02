592d719ae2e8f3228ecf1d030e39342761143b03
yuukiicode <yuukiicode@proton.me>
2025-11-02 22:58:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'coverartarchive.org',
+      'lastfm.freetls.fastly.net',
+      'i.scdn.co',
+      'upload.wikimedia.org',
+      'www.theaudiodb.com',
+    ],
+  },
+  env: {
+    LASTFM_API_KEY: process.env.LASTFM_API_KEY || 'YOUR_LASTFM_API_KEY',
+    HUGGINGFACE_API_KEY: process.env.HUGGINGFACE_API_KEY || '',
+  },
+}
+
+module.exports = nextConfig
 
