f43ebab04d559b8c74ced83fa41a6c24c10b7a34
yuukiicode <yuukiicode@proton.me>
2025-11-02 23:03:56 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="MuseLift - Inspire. Listen. Master. Discover timeless music and sharpen your musical ear." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
 
